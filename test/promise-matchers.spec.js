16b3ebed7fc47062c17c931d0171152b3a792d92
pietvanzoen <pietvanzoen@gmail.com>
2017-01-16 21:32:39 +0100

    feat(init): matcher accepts custom Promise implementation

--- test/promise-matchers.spec.js
+++ test/promise-matchers.spec.js
@@ -1,14 +1,53 @@
 var _ = require('lodash');
 var promiseMatchers = require('../lib/promise-matchers');
+var RSVP = require('rsvp');
 
-beforeEach(function() {
-  promiseMatchers.install(jasmine);
+describe('jasmine-promise-matchers', function() {
+
+  describe('install', function() {
+    beforeEach(function() {
+      delete jasmine.Expectation.prototype.toResolve;
+      delete jasmine.Expectation.prototype.toReject;
+    });
+
+    it('installs matchers on given jasmine instance', function(done) {
+      promiseMatchers.install({jasmine: jasmine});
+      expect(Promise.resolve()).toResolve(done);
+    });
+
+    it('uses given Promise implementation when given', function(done) {
+      function cb() {
+        return 'foo'
+      }
+      spyOn(RSVP.Promise, 'resolve').and.callFake(function(cbValue) {
+        expect(cbValue).toBe('foo');
+        done();
+      });
+      promiseMatchers.install({jasmine: jasmine, Promise: RSVP.Promise});
+      expect(Promise.resolve()).toResolve(done.fail, cb);
+    });
+
+    it('jasmine can be passed as the only argument', function(done) {
+      promiseMatchers.install(jasmine);
+      expect(Promise.resolve()).toResolve(done);
+    });
+  });
+
+  describe('matchers', function() {
+    testPromiseMatcher('resolve');
+    testPromiseMatcher('reject');
+  });
+
 });
 
 function testPromiseMatcher(promiseType) {
   var matcherName = 'to' + _.startCase(promiseType);
   var oppositePromiseType = promiseType === 'resolve' ? 'reject' : 'resolve';
   describe(matcherName, function() {
+
+    beforeEach(function() {
+      promiseMatchers.install(jasmine);
+    });
 
     it('passes with ' + _.trim(promiseType, 'e') + 'ed promises', function(done) {
       expect(Promise[promiseType]())[matcherName](done);
@@ -67,6 +106,3 @@ function testPromiseMatcher(promiseType) {
 
   });
 }
-
-testPromiseMatcher('resolve');
-testPromiseMatcher('reject');
